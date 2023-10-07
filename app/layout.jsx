07afa732901f3559402b7d3336756c994abf5deb
inviona <inviajna@gmail.com>
2023-10-07 22:25:45 +0200

    I just finished the project

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,26 +1,28 @@
-import '@styles/globals.css';
-import Nav from '@components/nav'
-import Provider from '@components/Provider';
+import "@styles/globals.css";
+
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+
 export const metadata = {
-    title: "Promptopia",
-    description: "Discover & share AI Prompts",
-}
-const RootLayout = ({ children }) => {
-  return (
-    <html lang="en">
-        <body>
-            <div className='main'>
-                <div className='gradient'/>
-            </div>
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
 
-            <main className='app'>
-                <Nav />
-                {children}
-            </main>
-        </body>
+const RootLayout = ({ children }) => (
+  <html lang='en'>
+    <body>
+      <Provider>
+        <div className='main'>
+          <div className='gradient' />
+        </div>
 
-    </html>
-  )
-}
+        <main className='app'>
+          <Nav />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
 
 export default RootLayout;
--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import Feed from '@components/feed'
-const Home = () => {
-  return (
-  <section className="w-full  flex-center flex-col">
-    <h1 className="head_text text-center">Discover & Share
-    <br className="max-md:hiden " />
-    <span className="orange_gradient text-center">AI-Powred Prompts</span>
-    </h1>
-    <p className="decs text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus earum molestias repellat fugit officiis quas, eligendi perspiciatis itaque non adipisci harum ea laboriosam neque fugiat cum omnis veritatis maxime voluptatibus.</p>
-    <Feed />   
-  </section>
-  
-  )
-}
+import Feed from "@components/Feed";
 
-export default Home
+const Home = () => (
+  <section className='w-full flex-center flex-col'>
+    <h1 className='head_text text-center'>
+      Discover & Share
+      <br className='max-md:hidden' />
+      <span className='orange_gradient text-center'> AI-Powered Prompts</span>
+    </h1>
+    <p className='desc text-center'>
+      Promptopia is an open-source AI prompting tool for modern world to
+      discover, create and share creative prompts
+    </p>
+
+    <Feed />
+  </section>
+);
+
+export default Home;
